df2e01f0cdf21f4b9488974f91ba5dffead65922
Marco Quintella <2om.marco.quintella@gmail.com>
2024-03-23 00:46:30 -0300

    feat :: Add Radio component

---
+++ components/B/Radio.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  label?: string
+  value: any
+  disabled?: boolean
+  name?: string
+}>()
+
+const model = defineModel()
+</script>
+
+<template>
+  <div flex cursor-pointer items-center gap-1>
+    <input v-model="model" :value="value" type="radio" :name="name">
+    <label v-if="label" :for="name">{{ label }}</label>
+  </div>
+</template>
+
+<style lang="sass">
+input[type="radio"]
+  @apply mt-0.5 cursor-pointer w-4 h-4
+
+  &[disabled]
+    @apply important-cursor-not-allowed
+</style>
