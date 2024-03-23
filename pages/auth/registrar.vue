fc68b4040f7d325317025db5dbae757219ffa130
Marco Quintella <2om.marco.quintella@gmail.com>
2024-03-22 23:49:08 -0300

    feat :: Styled Login and Register Forms

--- pages/auth/registrar.vue
+++ pages/auth/registrar.vue
@@ -1,26 +1,34 @@
 <script setup lang="ts">
 import { z } from 'zod'
+import type { Role } from '~/db/role'
 
 const model = reactive({
+  name: undefined as string | undefined,
   email: undefined as string | undefined,
   password: undefined as string | undefined,
+  role: 'candidate' as Role,
 })
 
-const isValid = computed(() => !!model.email && !!model.password)
+const isValid = computed(() => !!model.name && !!model.email && !!model.password)
 
-async function onSubmit() {
-  if (!model.email || !model.password)
+function onSubmit() {
+  if (!model.name || !model.email || !model.password)
     return
 
   if (z.string().email().safeParse(model.email).success === false)
     return
 
-  authLogin(model.email, model.password)
+  authRegister({
+    name: model.name,
+    email: model.email,
+    password: model.password,
+    role: model.role,
+  })
 }
 </script>
 
 <template>
-  <div class="w-full flex justify-center pt-10vh">
+  <div class="flex justify-center pt-10vh">
     <form
       max-w-100
       w-full
@@ -31,21 +39,22 @@ async function onSubmit() {
       p-8
       @submit.prevent="onSubmit"
     >
-      <h1>Login</h1>
+      <h1>Registrar</h1>
+      <b-input v-model="model.name" label="Nome" name="name" />
       <b-input v-model="model.email" label="E-mail" name="email" />
       <b-input v-model="model.password" label="Senha" name="password" type="password" />
       <b-button type="submit" primary :disabled="!isValid">
-        Entrar
+        Cadastrar
       </b-button>
       <p>
-        Ainda não tem cadastro? <nuxt-link hoverable to="/auth/registrar">
-          Registre-se!
+        Já é cadastrado? <nuxt-link hoverable to="/auth/login">
+          Entre aqui!
         </nuxt-link>
       </p>
     </form>
   </div>
 </template>
 
-<style lang="scss">
+<style lang="sass">
 
 </style>
