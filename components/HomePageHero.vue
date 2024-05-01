03bf2cf799ff7dd8c29c1f015de5790207a85562
Marco Quintella <2om.marco.quintella@gmail.com>
2024-04-30 22:49:01 -0300

    feat :: Hero Scaffold

---
+++ components/HomePageHero.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div b="1 primary rd-2" flex flex-col gap-4 p4>
+    <h1>
+      Na Recruitech está aqui para ajudar empresas e profissionais
+      a facilitar o seu processo de recrutamento e seleção.
+    </h1>
+    <div>
+      Mais e mais empresas utilizam processos rápidos em simples
+      na hora de contratar novos talentos. A Recruitech é uma plataforma
+      que conecta empresas e profissionais de maneira rápida e eficiente.
+    </div>
+    <div flex gap-8>
+      <q-btn color="secondary" rounded>
+        Recrutar
+      </q-btn>
+      <q-btn color="primary" rounded>
+        Buscar Vagas
+      </q-btn>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,23 +6,26 @@ definePageMeta({
 
 <template>
   <div w-full flex justify-center>
-    <div max-w-320 w-full flex flex-col gap-8 sm:flex-row>
-      <div w-full sm:max-w-80>
-        <div b="1 primary rd-2" flex flex-col gap-2 p-4>
-          <h2>Poste uma Vaga</h2>
-          <p class="text-balance">
-            Alcance profissionais da indústria tech de maneira facilitada e prática, sem abrir mão das práticas da sua empresa.
-          </p>
-          <ul>
-            <li>Anuncie por 28 dias</li>
-            <li>E-mails de alerta com candidatos em potencial</li>
-          </ul>
-          <q-btn color="primary" mt-2 rounded>
-            Postar Vaga
-          </q-btn>
+    <div max-w-320 w-full flex flex-col gap-8>
+      <HomePageHero w-full />
+      <div w-full flex flex-col gap-8 sm:flex-row>
+        <div w-full sm:max-w-80>
+          <div b="1 primary rd-2" flex flex-col gap-2 p-4>
+            <h2>Poste uma Vaga</h2>
+            <p class="text-balance">
+              Alcance profissionais da indústria tech de maneira facilitada e prática, sem abrir mão das práticas da sua empresa.
+            </p>
+            <ul>
+              <li>Anuncie por 28 dias</li>
+              <li>E-mails de alerta com candidatos em potencial</li>
+            </ul>
+            <q-btn color="primary" mt-2 rounded>
+              Postar Vaga
+            </q-btn>
+          </div>
         </div>
+        <LatestOpportunities flex-1 />
       </div>
-      <LatestOpportunities flex-1 />
     </div>
   </div>
 </template>
