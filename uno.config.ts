3fce516203e729092d9998e41e8a422e48e73c04
Marco Quintella <2om.marco.quintella@gmail.com>
2024-06-07 01:39:47 -0300

    chore :: add uno shortcuts for default border style

--- uno.config.ts
+++ uno.config.ts
@@ -29,6 +29,8 @@ export default defineConfig({
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+    ['border', 'b-2 b-rd-2'],
+    ['primary-border', 'border b-primary'],
   ],
   theme: {
     colors: {
