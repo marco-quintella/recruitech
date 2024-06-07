ef9cef6c6a01cc8e12f87377c74a5d956631cbae
Marco Quintella <2om.marco.quintella@gmail.com>
2024-06-07 01:40:32 -0300

    feat :: new company avatar component

---
+++ components/CompanyAvatar.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+defineProps<{ src?: string | null | undefined, name: string }>()
+</script>
+
+<template>
+  <q-avatar rounded color="primary" text-color="primary-text">
+    <img v-if="src" :src="src">
+    <template v-else>
+      {{ name }}
+    </template>
+  </q-avatar>
+</template>
